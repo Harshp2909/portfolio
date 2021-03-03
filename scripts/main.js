064eb72168b634ce3c06a3766e90a6305e57524e
Harsh Patel <Harshkp10477@gmail.com>
2021-03-02 21:57:36 -0500

    adding javascript and tech exp

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,5 @@ function welcome() {​​
     let username = prompt("What is your name?");    
     let message = "Welcome, " + username;
     document.getElementById('saywelcome').innerHTML = message; 
-
-}​​
+}
+​​
