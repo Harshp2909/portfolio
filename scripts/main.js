5e1a86102dd230a5972c708a05de4ae0cd4390aa
Harsh Patel <Harshkp10477@gmail.com>
2021-03-07 21:23:10 -0500

    Adding buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
-
+function f1(){
+    alert("Script 1 is in progress...");
+}
+function f2(){
+    alert("Script 2 is in progress...");
+}
+function f3(){
+    alert("Script 3 is in progress...");
+}
+function f4(){
+    alert("Script 4 is in progress...");
+}
+function f5(){
+    alert("Script 5 is in progress...");
+}
+function f6(){
+    alert("Script 6 is in progress...");
+}
+function f7(){
+    alert("Script 7 is in progress...");
+}
+function f8(){
+    alert("Script 8 is in progress...");
+}
+function f9(){
+    alert("Script 9 is in progress...");
+}
 
     
