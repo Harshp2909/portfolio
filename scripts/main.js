5a698a7ae7fc4e1667a3d63c6de3944e71e1bb45
Harsh Patel <Harshkp10477@gmail.com>
2021-03-08 22:29:50 -0500

    changing alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,29 @@
 function function1(){
-    alert("Script 1 is in progress...");
+    alert("Project 1 still in progress");
 }
 function function2(){
-    window.prompt("Script 2 is in progress...");
+    window.prompt("Project 2 is in progress");
 }
 function function3(){
-    alert("Script 3 is in progress...");
+    alert("Project 3 is in progress");
 }
 function function4(){
-    alert("Script 4 is in progress...");
+    alert("Project 4 is in progress");
 }
 function function5(){
-    alert("Script 5 is in progress...");
+    alert("Project 5 is in progress");
 }
 function function6(){
-    alert("Script 6 is in progress...");
+    alert("Project 6 is in progress");
 }
 function function7(){
-    alert("Script 7 is in progress...");
+    alert("Project 7 is in progress");
 }
 function function8(){
-    alert("Script 8 is in progress...");
+    alert("Project 8 is in progress");
 }
 function function9(){
-    alert("Script 9 is in progress...");
+    alert("Project 9 is in progress");
 }
 
     
